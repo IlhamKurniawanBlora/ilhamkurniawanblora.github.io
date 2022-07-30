92837e012e5d90d1c12cfdb50f62db54a3b3c0ff
ilhamkurniawanblora <ilhamkurniawanjateng@gmail.com>
2022-07-30 11:25:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content : ['./index.html'],
+    theme :{
+        container :{
+        center: true,
+        padding : "16px",
+        },
+        extend :{
+            colors:{
+                dark: '#0f172a',
+                primary: '#1e40af',
+                secondary: '#64748b'
+            },
+            screens :{
+            '2xl' : "1320px",
+            
+            },
+        },
+        
+        plugins :{
+
+        }
+    }
+}
